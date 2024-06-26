988a26533f801f5690cf28a97f4d545261cd329a
Alysson Ribeiro <wq15274059+sonalys@users.noreply.github.com>
2024-06-26 18:57:42 +0200

    Fix version error handling

--- src/api/version.ts
+++ src/api/version.ts
@@ -2,10 +2,10 @@ import env, { request } from './env';
 
 const version = async () : Promise<string> => {
   var resp = await request(`${env.appURL}/version`)
-  if (resp.status === 403) {
+  if (resp.status >= 400) {
     throw new Error("access denied");
   }
-  return await resp.text()
+  return resp.text()
 }
 
 export default version
